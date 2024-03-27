2396d51640f033b7f10fc4d37397979771160e7f
Luka Srdarev <70808068+burw0r@users.noreply.github.com>
2024-03-28 00:41:54 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # install.sh should be run without sudo
 
 sudo apt update
-sudo apt install -y i3 i3blocks alacritty terminator fonts-jetbrains-mono feh nitrogen picom npm flameshot
+sudo apt install -y i3 i3blocks alacritty terminator fonts-jetbrains-mono feh nitrogen picom npm flameshot rofi thunar
 sudo npm i -g alacritty-themes
 
 
