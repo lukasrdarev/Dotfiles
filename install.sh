f8b118468ef84cf62f89b78a884c369874cebda7
luka <luka.srdarev@gmail.com>
2024-10-08 20:16:03 +0200

    /

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ mkdir -p ~/.config/alacritty/themes
 mkdir -p ~/.config/i3/
 mkdir -p ~/Pictures/wallpapers/
 mkdir -p ~/.config/rofi/
-#mkdir -p ~/.config/kitty/
+mkdir -p ~/.config/kitty/
 mkdir -p ~/.config/picom/
 mkdir -p ~/.config/terminator
 
@@ -25,7 +25,7 @@ unzip "$HOME/Pictures/wallpapers/wallpapers.zip" -d "$HOME/Pictures/wallpapers/"
 cp "tmux/.tmux.conf"             "$HOME/.tmux.conf"
 cp "rofi/config.rasi"            "$HOME/.config/rofi/config.rasi"
 cp "terminator/config"		 "$HOME/.config/terminator/config"
-#cp "kitty/kitty.conf"            "$HOME/.config/kitty/kitty.conf"
+cp "kitty/kitty.conf"            "$HOME/.config/kitty/kitty.conf"
 sudo cp ./randomtheme.sh /usr/local/bin
 
 #echo 'PS1="%F{blue}%n%f %F{red}%~%f >> "' >> ~/.zshrc
