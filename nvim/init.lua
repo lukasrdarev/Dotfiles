9bcdfebb842c2812aa570955525c635e099072a8
Luka Srdarev <burw0r@macbook.local>
2025-03-22 23:26:41 +0100

    /

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,12 +3,16 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- ========== [general] ==========
+-- ========== [lazy package manager] ==========
 require("config.lazy")
 
 -- ========== [general] ==========
 vim.opt.number = true 
 vim.opt.relativenumber = true
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 
 -- ========== [theme] ==========
 vim.cmd("colorscheme aura-dark")
