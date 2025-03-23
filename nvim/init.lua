c80f7aa7f0bef5ff975b9c0c40802297a879bb9d
Luka Srdarev <burw0r@macbook.local>
2025-03-23 14:10:43 +0100

    /

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,22 +1,8 @@
 
--- ========== [Leader key remap] ==========
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
 
 -- ========== [lazy package manager] ==========
 require("config.lazy")
 
--- ========== [general] ==========
-vim.opt.number = true 
-vim.opt.relativenumber = true
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
--- ========== [theme] ==========
-vim.cmd("colorscheme aura-dark")
---vim.cmd("colorscheme retrobox")
--- use terminal background color
-vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+-- ========== [general settings] ==========
+require("config.vim-opts")
 
