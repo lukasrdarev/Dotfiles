5c60489b1cfe753f47ebf31fa24afc83f07b1d2c
Luka Srdarev <70808068+lukasrdarev@users.noreply.github.com>
2025-05-28 13:07:43 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # install.sh should be run without sudo
 
 sudo apt update
-sudo apt install -y i3 i3blocks alacritty terminator fonts-jetbrains-mono feh  picom flameshot rofi thunar kitty ripgrep
+sudo apt install -y i3 i3blocks alacritty terminator fonts-jetbrains-mono feh  picom flameshot rofi thunar kitty ripgrep vim neovim
 
 
 # TODO: add suport for arch,debin,fedora
